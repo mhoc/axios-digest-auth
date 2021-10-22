066e7179488c602edde271264f8575aa139c05bf
Koushik Dutta <koushd@gmail.com>
2021-10-22 13:07:28 -0700

    Use RFC compliant auth header parser.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import * as crypto from "crypto";
 import * as url from "url";
 import * as axios from "axios";
+import * as authHeader from 'auth-header';
+
+// from auth-header, but not exposed
+const quote = (str: string): string => `"${str.replace(/"/g, '\\"')}"`;
 
 /**
  * Options to configure the AxiosDigestAuth instance.
@@ -41,19 +45,43 @@ export default class AxiosDigestAuth {
       ) {
         throw resp1;
       }
-      const authDetails = resp1.response.headers['www-authenticate'].split(',').map((v: string) => v.split('='));
+
+      // const authDetails = resp1.response.headers['www-authenticate'].split(',').map((v: string) => v.split('='));
+
+      const wwwAuthenticate = resp1.response.headers['www-authenticate'];
+      const parsedAuthorization = authHeader.parse(wwwAuthenticate);
+
+
       ++this.count;
       const nonceCount = ('00000000' + this.count).slice(-8);
       const cnonce = crypto.randomBytes(24).toString('hex');
-      const realm = authDetails.find((el: any) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
-      const nonce = authDetails.find((el: any) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
+
+      // const realm = authDetails.find((el: any) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
+      const realm = parsedAuthorization.params['realm'];
+
+      // const nonce = authDetails.find((el: any) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
+      const nonce = parsedAuthorization.params['nonce'];
+
       const ha1 = crypto.createHash('md5').update(`${this.username}:${realm}:${this.password}`).digest('hex');
       const path = url.parse(opts.url!).pathname;
       const ha2 = crypto.createHash('md5').update(`${opts.method ?? "GET"}:${path}`).digest('hex');
       const response = crypto.createHash('md5').update(`${ha1}:${nonce}:${nonceCount}:${cnonce}:auth:${ha2}`).digest('hex');
-      const authorization = `Digest username="${this.username}",realm="${realm}",` +
-        `nonce="${nonce}",uri="${path}",qop="auth",algorithm="MD5",` +
-        `response="${response}",nc="${nonceCount}",cnonce="${cnonce}"`;
+
+      const params = {
+        username: this.username,
+        realm,
+        nonce,
+        uri: path || '',
+        qop: 'auth',
+        algorithm: 'MD5',
+        response,
+        nc: nonceCount,
+        cnonce,
+      };
+
+      const paramsString = Object.entries(params).map(([key, value]) =>  `${key}=${value && quote(value as string)}`).join(', ');
+      const authorization = `Digest ${paramsString}`;
+
       if (opts.headers) {
         opts.headers["authorization"] = authorization;
       } else {
